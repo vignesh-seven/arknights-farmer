3c241564dbe2e9f505124a497b5ef87defa34047
Vignesh <vignesh77024@gmail.com>
2022-11-29 19:21:15 +0530

    ok

--- farmer.py
+++ farmer.py
@@ -3,14 +3,16 @@ from ppadb.client import Client
 
 os.system("adb start-server")
 pwd = os.getcwd()
+screenshots = pwd + "\screenshots"
 
 client = Client(host="127.0.0.1", port=5037)
 device = client.device("emulator-5554")
 
-print("hello")
+print("Arknights Auto Farmer!")
 print(pwd)
 
 # create the screenshots folder (if it doesnt exist already)
-if not os.path.exists(pwd + "\screenshots"):
-    os.mkdir(pwd + "\screenshots")
+if not os.path.exists(screenshots):
+    os.mkdir(screenshots)
     print("Created the screenshots folder")
+
