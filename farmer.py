e84ba2c2847c5f64810db2b281365d2fd1d597e2
Vignesh <vignesh77024@gmail.com>
2022-11-29 19:19:06 +0530

    creates screenshot folder

--- farmer.py
+++ farmer.py
@@ -11,6 +11,6 @@ print("hello")
 print(pwd)
 
 # create the screenshots folder (if it doesnt exist already)
-if (os.path.exists(pwd + "\screenshots")):
+if not os.path.exists(pwd + "\screenshots"):
     os.mkdir(pwd + "\screenshots")
     print("Created the screenshots folder")
