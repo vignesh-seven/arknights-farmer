e75166c2a2d7eaffc56b52e843cf927ec94898ec
Vignesh <vignesh77024@gmail.com>
2022-11-29 19:17:06 +0530

    aaaa

--- farmer.py
+++ farmer.py
@@ -9,3 +9,8 @@ device = client.device("emulator-5554")
 
 print("hello")
 print(pwd)
+
+# create the screenshots folder (if it doesnt exist already)
+if (os.path.exists(pwd + "\screenshots")):
+    os.mkdir(pwd + "\screenshots")
+    print("Created the screenshots folder")
